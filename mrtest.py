224c2ba0073ce886ddd7bd58411d5221ca703f48
Ulrich Lukas <none@domain.invalid>
2019-11-21 19:17:53 +0100

    sc

--- mrtest.py
+++ mrtest.py
@@ -11,6 +11,10 @@ import re
 with open("tmp/netlist_1.cir", "rt") as f:
     netlist = f.read()
 
+test_table = (".func f_C_ISS(v_DG) {TABLE(v_DG, 0.0, 9622.0E-12, 6.6,"
+              "6088.0E-12, 15.9, 5038.7E-12, 28.9, 4741.4E-12, 47.5, "
+              "4499.5E-12, 85.0, 4401.8E-12, 601.2, 4469.3E-12)}")
+
 replacements = {"$r_1": "Foo_123"}
 replacements_wo = {"r_1": "Foo_123"}
 
@@ -20,19 +24,37 @@ class SpiceTemplate(Template):
 
 
 class SpiceConversions():
-    si_map = {"a": "E-18", "f": "E-15", "p": "E-12", "n": "E-9", "u": "E-6", 
+    si_map = {None: "", "f": "E-15", "p": "E-12", "n": "E-9", "u": "E-6", 
               "m": "E-3", "k": "E3", "meg": "E6", "g": "E9", "t": "E12"}
-    _suffix_re = re.compile(r"(\d*\.?\d+)([afpnumkgt]|(?:meg))", re.IGNORECASE)
+    # matches valid spice numbers with optional scale suffix and unit
+    _suffix_re = re.compile(r"[^a-z\d](\d*\.?\d+)([fpnumkgt]|(?:meg))?[a-z]*",
+                            re.IGNORECASE)
     _tuple_re = re.compile(r"((?:\d*\.?\d+)(?:[eE][+-]?\d+)?),[ \t]*"
                            r"((?:\d*\.?\d+)(?:[eE][+-]?\d+)?)")
 
-    def spice_to_float_str(self, spice_number_str: str) -> str:
-        return self._suffix_re.sub(lambda m: m[1] + self.si_map[m[2]],
-                                   spice_number_str)
+    def netlist_to_float(self, netlist: str) -> str:
+        """Convert all numbers in a spice-compatible input string
+        (e.g. a netlist) into valid floating point number strings.
+        
+        This removes all units from the number strings as well.
+        """
+        return self._suffix_re.sub(lambda m: m[1] + self.si_map[m[2]], netlist)
     
-    def spice_to_list(self, spice_number_str: str):
-        float_str = self.spice_to_float_str(spice_number_str)
+    def table_to_tuples(self, spice_table: str):
+        """Render the interleaved values from a spice piece-wise linear
+        lookup table ("TABLE") as a list of tuples.
+        """
+        float_str = self.netlist_to_float(spice_table)
         matches_iterator = self._tuple_re.finditer(float_str)
-        return [[float(m[1]), float(m[2])] for m in matches_iterator]
+        return [(float(m[1]), float(m[2])) for m in matches_iterator]
+
+    def table_to_lists(self, spice_table: str):
+        """Render the interleaved values from a spice piece-wise linear
+        lookup table ("TABLE") as two lists of corresponding X and Y data.
+        
+        Output is a tuple of these two lists
+        """
+        tuples = self.table_to_tuples(spice_table)
+        return ([i for i, j in tuples], [j for i, j in tuples])
 
 sc = SpiceConversions()
